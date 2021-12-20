92b48627e5641e11a303f9bd88988942d50fabbe
”Grisha <lookgreg@yandex.ru>
2021-12-20 23:29:38 +0300

    task_done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,23 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    // console.log(name, phone, address, goods, sum)
+    let data = {client: "", order: {}, goods: []};
+    data.client = `${name} ${phone}`;
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < goods.length; i += 1) {
+        let item = {};
+        item.title = goods[i].title;
+        item.count = goods[i].count;
+        data.goods.push(item);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
+    console.log(jsonData)
     return jsonData;
 }
